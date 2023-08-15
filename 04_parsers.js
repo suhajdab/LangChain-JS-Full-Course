93130dd8cedee2b20b5644eec0b3a48270b4983f
Balázs Suhajda <suhajdab@gmail.com>
2023-08-15 21:03:20 +0200

    Minor improvements, delimiter

--- 04_parsers.js
+++ 04_parsers.js
@@ -12,12 +12,14 @@ const formatInstructions = parser.getFormatInstructions();
 
 const prompt = new PromptTemplate({
   template:
-    "Be very funny when answering questions\n{format_instructions}\n Question: {question}",
+    `Be absolutely hilarious when answering questions!
+    Question: <<<{question}>>>
+    {format_instructions}`,
   inputVariables: ["question"],
   partialVariables: { format_instructions: formatInstructions },
 });
 
-const model = new OpenAI({ temperature: 0 });
+const model = new OpenAI({ temperature: 0.9 });
 
 const input = await prompt.format({
   question: "What is the capital of France?",
@@ -25,7 +27,6 @@ const input = await prompt.format({
 console.log(input);
 
 const response = await model.call(input);
-
-console.log(response);
-
-console.log(await parser.parse(response));
+console.log("string", response);
+const responseObj = await parser.parse(response);
+console.log("object", responseObj);
